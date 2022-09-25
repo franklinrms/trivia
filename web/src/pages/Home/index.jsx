5335b2e72cf193f73dea7cb7d086edefff9c3678
Franklin Ramos <franklinramos@outlook.com>
2022-09-25 17:34:24 -0300

    feat: create inputs form

--- web/src/pages/Home/index.jsx
+++ web/src/pages/Home/index.jsx
@@ -1,7 +1,11 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { getToken } from '../../lib/api';
 
 export default function Home() {
+  const [player, setPlayer] = useState({
+    name: '', score: 0, assertions: 0, gravatarEmail: '',
+  });
+
   useEffect(() => {
     const fetchToken = async () => {
       const token = await getToken();
@@ -10,7 +14,29 @@ export default function Home() {
     fetchToken();
   }, []);
 
+  const handleChange = ({ target: { name, value } }) => {
+    setPlayer({ ...player, [name]: value });
+  };
+
   return (
-    <div>index</div>
+    <div>
+      <form>
+        <h1>Trivia</h1>
+
+        <input
+          placeholder="Type your e-mail"
+          type="text"
+          name="gravatarEmail"
+          onChange={handleChange}
+        />
+
+        <input
+          placeholder="Type your name"
+          type="text"
+          name="name"
+          onChange={handleChange}
+        />
+      </form>
+    </div>
   );
 }
