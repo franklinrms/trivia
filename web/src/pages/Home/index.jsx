6a15354a652a42626c078d9ea5e8c5e30c0f2657
Franklin Ramos <franklinramos@outlook.com>
2022-09-25 21:14:10 -0300

    feat: home page

--- web/src/pages/Home/index.jsx
+++ web/src/pages/Home/index.jsx
@@ -1,10 +1,21 @@
 import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import { getToken } from '../../lib/api';
+import { setPlayerValues } from '../../reduce/actions';
+// import art from '../../assets/art.png';
+import {
+  Buttons, Container, Form, PlayButton,
+} from './style';
+import bg from '../../assets/bg.png';
 
 export default function Home() {
   const [player, setPlayer] = useState({
     name: '', score: 0, assertions: 0, gravatarEmail: '',
   });
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchToken = async () => {
@@ -18,9 +29,22 @@ export default function Home() {
     setPlayer({ ...player, [name]: value });
   };
 
+  const onSubmitButton = () => {
+    const { name, gravatarEmail } = player;
+    if (name && gravatarEmail) {
+      dispatch(setPlayerValues(player));
+    }
+    navigate('/game');
+  };
+
   return (
-    <div>
-      <form>
+    <Container bg={bg}>
+      {/* <img src={art} alt="art" /> */}
+      <Form onSubmit={(event) => {
+        event.preventDefault();
+        onSubmitButton();
+      }}
+      >
         <h1>Trivia</h1>
 
         <input
@@ -36,7 +60,19 @@ export default function Home() {
           name="name"
           onChange={handleChange}
         />
-      </form>
-    </div>
+
+        <PlayButton type="submit">Play</PlayButton>
+        <span>
+          <Buttons type="button" onClick={() => navigate('/settings')}>
+            <i className="bx bx-cog bx-tada-hover" />
+            Settings
+          </Buttons>
+          <Buttons type="button" onClick={() => navigate('/ranking')}>
+            <i className="bx bx-trophy bx-tada-hover" />
+            Ranking
+          </Buttons>
+        </span>
+      </Form>
+    </Container>
   );
 }
---
+++ web/src/pages/Home/style.jsx
@@ -0,0 +1,111 @@
+import styled from 'styled-components';
+import theme from '../../style/theme';
+
+export const Container = styled.div`
+    display: flex;
+    width: 100%;
+    height: 100vh;
+    padding: 20px;
+    background-color: ${theme.colors.brand};
+    background-image: url(${({ bg }) => bg});
+    justify-content: space-around;
+    align-items: center;
+
+    img {
+        width: 30%;
+    }
+`;
+
+export const Form = styled.form`
+    background-color: ${theme.colors.background};
+    display: flex;
+    flex-direction: column;
+    border-radius: 30px;
+    width: 30%;
+    height: 70%;
+    justify-content: center;
+    align-items: center;
+
+    h1 {
+        font-size: 46px;
+        margin-bottom: 30px;
+    }
+    input {
+        width: 80%;
+        padding: 15px;
+        border: 4px solid ${theme.colors.brand};
+        border-radius: 20px;
+        font-size: 24px;
+        margin-bottom: 20px;
+    }
+    span {
+        width: 80%;
+        display: flex;
+        justify-content: space-evenly;
+    }
+
+    @media (max-width: 850px) {
+        justify-content: space-evenly;
+        align-items: center;
+        width: 100%;
+        height: 85%;
+    }
+`;
+
+export const PlayButton = styled.button`
+    width: 80%;
+    padding: 10px;
+    font-size: 30px;
+    font-weight: bold;
+    border: 4px solid ${theme.colors.brand};
+    border-radius: 20px;
+    background: ${theme.colors.brand};
+    transition: all 0.3s ease;
+    position: relative;
+    display: inline-block;
+    z-index: 1;
+    margin-bottom: 20px;
+    margin-top: 20px;
+    letter-spacing: 1px;
+
+    &::after{
+        position: absolute;
+        content: "";
+        width: 0;
+        height: 100%;
+        top: 0;
+        right: 0;
+        z-index: -1;
+        background: ${theme.colors.background};
+        border-radius: 20px;
+        transition: all 0.3s ease;
+    }
+    &:hover::after {
+        left: 0;
+        width: 100%;
+    }
+    &:active{
+        top: 2px;
+    }
+`;
+
+export const Buttons = styled.button`
+    background: transparent;
+    margin-top: 30px;
+    padding: 10px;
+    letter-spacing: 1px;
+    font-size: 24px;
+    border: none;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 5px;
+    color: ${theme.colors.text_secondary};
+
+    &:active{
+        top: 2px;
+    }
+    i {
+        color: ${theme.colors.text_secondary};
+    }
+`;
