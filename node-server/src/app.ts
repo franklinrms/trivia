9d0d21f027c7a6447d8bff871db971630fa7bf80
Franklin Ramos <franklinramos@outlook.com>
2022-09-28 13:00:12 -0300

    fix: add cors

--- node-server/src/app.ts
+++ node-server/src/app.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 import errorMiddleware from './middleware/httpErroMiddleware';
 import RankingController from './controllers/rankingController';
 
 const app = express();
 const rankingController = new RankingController();
 
+app.use(cors());
 app.use(express.json());
 
 app.post('/ranking', rankingController.create);
